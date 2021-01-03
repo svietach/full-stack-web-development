76edcfa3504966de9e8c9d0126e5031f4f65ba7a
Svitlana Chaplinska <ch.svieta@gmail.com>
2021-01-04 00:11:18 +0200

    Issue has been fixed

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -97,6 +97,6 @@ app.get('/info', (req, res) => {
 
 const PORT = process.env.PORT || 3001
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}.`)
 })
