376377c371015bedff1a681ced614a96b5d5a575
Svitlana Chaplinska <ch.svieta@gmail.com>
2021-01-05 21:58:29 +0200

    Exercises 4.12-4.14 completed :tada:

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -9,6 +9,32 @@ blogsRouter.get('/api/blogs', (request, response) => {
         })
 })
 
+blogsRouter.delete('/api/blogs/:id', async (request, response) => {
+    const findBlog = await Blog.findById(request.params.id);
+    if (!!findBlog) {
+        Blog.findByIdAndRemove(request.params.id, (err, result) => {
+            if (err) {
+                response.status(500).json({ error: 'Delete failed!' });
+            } else {
+                response.status(200).json({ message: 'Blog deleted!' });
+            }
+        });
+    } else {
+        response.status(404).json({ error: 'Blog doesn\'t exist!' });;
+    }
+})
+
+blogsRouter.put('/api/blogs/:id', (request, response) => {
+    const { author, title, url, likes } = request.body;
+    Blog.findByIdAndUpdate(request.params.id, { author, title, url, likes }, (err, docs) => {
+        if (err) {
+            response.status(500).json({ error: 'Update failed!' });
+        } else {
+            response.status(200).json({ error: 'Blog successfully!' });
+        }
+    });
+})
+
 blogsRouter.post('/api/blogs', (request, response) => {
     const { title, url } = request.body;
     if (!title && !url) {
--- part4/blogList/supertest.test.js
+++ part4/blogList/supertest.test.js
@@ -46,10 +46,43 @@ test('verifies that likes by default to 0', async () => {
         expect(item.likes).toBeGreaterThanOrEqual(0);
     })
 })
+
 test('verifies that title and url properties are missing from the request and triggers 400 status code', async () => {
     const responsePost = await api.post('/api/blogs')
         .send({ author: 'Harry Potter', likes: 999 })
         .expect(400);
+})
+
+test('verifies that delete endpoint removes blogs correctly', async () => {
+    const responseGetBeforeDelete = await api.get('/api/blogs');
+
+    const responseGetLengthBeforeDelete = responseGetBeforeDelete.body.length;
+
+    const responseDelete = await api.delete(`/api/blogs/${responseGetBeforeDelete.body[0]["_id"]}`)
+        .expect(200);
+
+    const responseGetAfterDelete = await api.get('/api/blogs');
+
+    expect(responseGetLengthBeforeDelete).toBeGreaterThan(responseGetAfterDelete.body.length);
+})
+
+test('verifies that delete endpoint removes blogs correctly', async () => {
+    const responseGetBeforeUpdate = await api.get('/api/blogs');
+
+    const responseUpdate = await api.put(`/api/blogs/${responseGetBeforeUpdate.body[0]["_id"]}`)
+        .send({ author: 'test', title: 'test', url: 'test', likes: '777' })
+        .expect(200);
+
+    const responseGetAfterUpdate = await api.get('/api/blogs');
+
+    expect(responseGetAfterUpdate.body[0]).toEqual({
+        author: 'test',
+        title: 'test',
+        url: 'test',
+        likes: 777,
+        __v: 0,
+        _id: responseGetBeforeUpdate.body[0]["_id"]
+    });
 })
 
 afterAll(() => {
